b74640df2029a3a913fb56c50abaf38a4c0dc7dc
xiao2230 <xiaobow2230@gmail.com>
2021-10-19 15:57:01 +0800

    完成關於我們內頁

--- FlowerMessenger/js/aboutUs.js
+++ FlowerMessenger/js/aboutUs.js
@@ -1,16 +1,34 @@
 let headerNav = document.querySelector('.header_nav');
+let cornerPic = document.querySelector('.content_corner img');
+let cornerText = document.querySelectorAll('.content_corner .cornerText p');
+let cornerTextBgFlower = document.querySelector('.cornerTextBgFlower');
 
 //用來定特效出現的高度參考點
+let header = document.querySelector('.header');
+let bannerTitle = document.querySelector('.header_bannerTitle .tt1');
 let content = document.querySelector('.content');
 
 document.addEventListener('scroll', () => {
-    let scrollTop = document.documentElement.scrollTop;
+	let scrollTop = document.documentElement.scrollTop;
+	
+	if (scrollTop >= header.offsetTop) {
+		// 『主要內容區-店內角落圖&內文&花朵飄散背景』的主題圖漸入
+		cornerPic.classList.add('appear');
+	}
+	if (scrollTop >= bannerTitle.offsetTop) {
+		// 『主要內容區-店內角落圖&內文&花朵飄散背景』的內文漸入
+		for (i = 0, t = 0; i < cornerText.length; i++, t += Math.random() * 500) {
+			setTimeout(`cornerText[${i}].classList.add('locate01')`, t);
+		}
+		// 『主要內容區-店內角落圖&內文&花朵飄散背景』的花朵飄散
+		cornerTextBgFlower.classList.add('locate02');
+	}
 
-    if (scrollTop >= content.offsetTop) {
-        // 『頂部內容區-導覽列』的新增BackToTop
-        headerNav.classList.add('navPlusTop');
-    } else {
-        // 『頂部內容區-導覽列』的移除BackToTop
-        headerNav.classList.remove('navPlusTop');
-    }
-})
+	if (scrollTop >= content.offsetTop) {
+		// 『頂部內容區-導覽列』的新增BackToTop
+		headerNav.classList.add('navPlusTop');
+	} else {
+		// 『頂部內容區-導覽列』的移除BackToTop
+		headerNav.classList.remove('navPlusTop');
+	}
+});
--- FlowerMessenger/js/index.js
+++ FlowerMessenger/js/index.js
@@ -8,68 +8,64 @@ let takeCareContentText = document.querySelectorAll('.takeCareContentText p');
 let takeCareContentPic = document.querySelector('.takeCareContentPic div');
 let headerNav = document.querySelector('.header_nav');
 
-
-
 //用來定特效出現的高度參考點
 let content = document.querySelector('.content');
 let contentFlowerBuySecond = document.querySelector('.content_flowerBuy ul li:nth-child(2)');
 let contentFlowerBuyLast = document.querySelector('.content_flowerBuy ul li:last-child');
 
-
-
 document.addEventListener('scroll', () => {
-    let scrollTop = document.documentElement.scrollTop;
+	let scrollTop = document.documentElement.scrollTop;
 
-    // 『主要內容區-關於我們』的滑入動態
-    if (scrollTop >= content.offsetTop / 3) {
-        for (i = 0, t = 0; i < aboutUs.length; i++, t += 200) {
-            // 第一格要使用引號包起來，但會將原本須帶入的變數一起包住，造成無法讀取for迴圈的i值，所以有兩種寫法
-            //第一種："aboutUs["+i+"].classList.add('aboutUsIn')"
-            //第二種用ES6的模板字串符 template literal，如下：
-            setTimeout(`aboutUs[${i}].classList.add('aboutUsIn')`, t)
-        }
-        css.insertRule(".content_aboutUs::before{opacity: .3;height: 85%;}", 0);
-        fly[0].classList.add('flygo01');
-        fly[1].classList.add('flygo02');
-        h2[0].classList.add('appear');
-    }
+	// 『主要內容區-關於我們』的滑入動態
+	if (scrollTop >= content.offsetTop / 3) {
+		for (i = 0, t = 0; i < aboutUs.length; i++, t += 200) {
+			// 第一格要使用引號包起來，但會將原本須帶入的變數一起包住，造成無法讀取for迴圈的i值，所以有兩種寫法
+			//第一種："aboutUs["+i+"].classList.add('aboutUsIn')"
+			//第二種用ES6的模板字串符 template literal，如下：
+			setTimeout(`aboutUs[${i}].classList.add('aboutUsIn')`, t);
+		}
+		css.insertRule('.content_aboutUs::before{opacity: .3;height: 85%;}', 0);
+		fly[0].classList.add('flygo01');
+		fly[1].classList.add('flygo02');
+		h2[0].classList.add('appear');
+	}
 
-    if (scrollTop >= content.offsetTop) {
-        // 『頂部內容區-導覽列』的新增置頂
-        headerNav.classList.add('navFixedBg');
+	if (scrollTop >= content.offsetTop) {
+		// 『頂部內容區-導覽列』的新增置頂
+		headerNav.classList.add('navFixedBg');
 
-        // 『主要內容區-商品類別』前兩項的滑入動態
-        for (i = 0; i < flowerBuy.length / 2; i++) {
-            flowerBuy[i].classList.add('appear');
-        }
-        for (i = 0; i < flowerBuyPic.length / 2; i++) {
-            flowerBuyPic[i].classList.add('locate01');
-        }
-        h2[1].classList.add('appear');
-    } else {
-        // 『頂部內容區-導覽列』的移除置頂
-        headerNav.classList.remove('navFixedBg');
-    }
+		// 『主要內容區-商品類別』前兩項的滑入動態
+		for (i = 0; i < flowerBuy.length / 2; i++) {
+			flowerBuy[i].classList.add('appear');
+		}
+		for (i = 0; i < flowerBuyPic.length / 2; i++) {
+			flowerBuyPic[i].classList.add('locate01');
+		}
+		h2[1].classList.add('appear');
+	} else {
+		// 『頂部內容區-導覽列』的移除置頂
+		headerNav.classList.remove('navFixedBg');
+	}
 
-    // 『主要內容區-商品類別』後兩項的滑入動態
-    if (scrollTop >= contentFlowerBuySecond.offsetTop) {
-        for (i = flowerBuy.length / 2; i < flowerBuy.length; i++) {
-            flowerBuy[i].classList.add('appear');
-        }
+	// 『主要內容區-商品類別』後兩項的滑入動態
+	if (scrollTop >= contentFlowerBuySecond.offsetTop) {
+		for (i = flowerBuy.length / 2; i < flowerBuy.length; i++) {
+			flowerBuy[i].classList.add('appear');
+		}
 
-        for (i = flowerBuyPic.length / 2; i < flowerBuyPic.length; i++) {
-            flowerBuyPic[i].classList.add('locate01');
-        }
-    }
+		for (i = flowerBuyPic.length / 2; i < flowerBuyPic.length; i++) {
+			flowerBuyPic[i].classList.add('locate01');
+		}
+	}
 
-    // 『主要內容區-花朵照護』的滑入動態
-    if (scrollTop >= contentFlowerBuyLast.offsetTop) {
-        h2[2].classList.add('appear');
-        takeCareContentPic.classList.add('locate03');
-        css.insertRule(".takeCareContentText::after{opacity: .3;top:30px;}", 0);
+	// 『主要內容區-花朵照護』的滑入動態
+	if (scrollTop >= contentFlowerBuyLast.offsetTop) {
+		h2[2].classList.add('appear');
+		takeCareContentPic.classList.add('locate03');
+		css.insertRule('.takeCareContentText::after{opacity: .3;top:30px;}', 0);
 
-        for (i = 0, t = 0; i < takeCareContentText.length; i++, t += Math.random() * 500) {
-            setTimeout(`takeCareContentText[${i}].classList.add('locate02')`, t)
-        }
-    }
-})
+		for (i = 0, t = 0; i < takeCareContentText.length; i++, t += Math.random() * 500) {
+			setTimeout(`takeCareContentText[${i}].classList.add('locate02')`, t);
+		}
+	}
+});
