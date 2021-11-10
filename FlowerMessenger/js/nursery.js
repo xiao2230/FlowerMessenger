ba52d7afcc4e1142bf8abc8984f6ae5a46408222
xiao2230 <xiaobow2230@gmail.com>
2021-11-10 13:13:15 +0800

    首頁動畫時間修改、完成花草養護

--- FlowerMessenger/js/nursery.js
+++ FlowerMessenger/js/nursery.js
@@ -1,10 +1,23 @@
 let headerNav = document.querySelector('.header_nav');
+let contentPreface = document.querySelector('.content_preface');
+let contentIllustration = document.querySelector('.content_illustration');
+let contentArticle = document.querySelector('.content_article');
+let contentConclusion = document.querySelector('.content_conclusion');
 
 //用來定特效出現的高度參考點
+let header = document.querySelector('.header');
 let content = document.querySelector('.content');
 
 document.addEventListener('scroll', () => {
     let scrollTop = document.documentElement.scrollTop;
+
+    if (scrollTop > header.offsetTop) {
+        // 『主要內容區』的內容漸入
+        contentPreface.classList.add('appear');
+        contentIllustration.classList.add('appear');
+        contentArticle.classList.add('appear');
+        contentConclusion.classList.add('appear');
+    }
 
     if (scrollTop > content.offsetTop) {
         // 『頂部內容區-導覽列』的新增BackToTop
