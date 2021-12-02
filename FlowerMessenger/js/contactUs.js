e458f2fcbd320ddd2bca5bdc040eb3aa674581a8
xiao2230 <xiaobow2230@gmail.com>
2021-12-02 15:51:27 +0800

    聯絡我們 補上必填

--- FlowerMessenger/js/contactUs.js
+++ FlowerMessenger/js/contactUs.js
@@ -1,9 +1,19 @@
 let headerNav = document.querySelector('.header_nav');
-let cUBtnSend = document.getElementById('cUBtnSend');
-let cUConfirm = document.getElementById('cUConfirm');
+let realNameInput = document.querySelector('.content_cUForm form .cURealName input');
+let telInput = document.querySelector('.content_cUForm form .cUTel input');
+let questionTextarea = document.querySelector('.content_cUForm form .cUQuestion textarea');
+let confirmBtn = document.querySelector('.content_cUForm form .cUFormSubmit input:first-child');
+let submitInput = document.querySelector('.content_cUForm form .cUFormSubmit input:last-child')
 
 //用來定特效出現的高度參考點
 let content = document.querySelector('.content');
+
+document.addEventListener('DOMContentLoaded', () => {
+	// 『主要內容區-提問表格』預設部分欄位必填
+	realNameInput.setAttribute('required',true)
+	telInput.setAttribute('required',true)
+	questionTextarea.setAttribute('required',true)
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     // 『主要內容區』的內容漸入定位
@@ -22,8 +32,8 @@ document.addEventListener('scroll', () => {
 	}
 });
 
-// 『主要內容區 - 提問表格』控制表單送出按鈕是否啟用
-cUBtnSend.setAttribute('disabled',true);
-cUConfirm.addEventListener('click', () => {
-	cUBtnSend.toggleAttribute('disabled');
+// 『主要內容區-提問表格』控制表單送出按鈕是否啟用
+submitInput.setAttribute('disabled',true);
+confirmBtn.addEventListener('click', () => {
+	submitInput.toggleAttribute('disabled');
 });
