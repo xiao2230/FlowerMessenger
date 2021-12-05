a2ec29b21bdebd7fca237736793a4796d8143190
xiao2230 <xiaobow2230@gmail.com>
2021-12-05 17:00:22 +0800

    月曆進度增加

--- FlowerMessenger/js/obSmallSeries.js
+++ FlowerMessenger/js/obSmallSeries.js
@@ -92,6 +92,7 @@ Vue.createApp({
 					name: 'oBNum'
 				},
 				calendar: {
+					val: '',
 					id: 'oBCal',
 					name: 'oBCal'
 				},
@@ -117,18 +118,18 @@ Vue.createApp({
 			},
 			// 月曆：資料
 			calendarData:{
-				calMY:'',
-				showCalBtn:'',
-				calMYLeftSwitch:'',
-				calMYRightSwitch:'',
-				days:['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+				calMonthYear:'',
+				calDays:['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+				classShowCal:'',
+				classLeftSwitch:'',
+				classRightSwitch:'',
 				data01:{
-					mY:'September 2021',
-					d:['','','',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,'']
+					monthYear:'September 2021',
+					date:['','','',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,'']
 				},
 				data02:{
-					mY:'January 2022',
-					d:['','','','','','',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,'','','','','']
+					monthYear:'January 2022',
+					date:['','','','','','',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,'','','','','']
 				}
 			}
 		};
@@ -142,10 +143,8 @@ Vue.createApp({
 
 				for (i = 0; i < this.bouquetPic.length; i++) {
 					this.bouquetPic[i].mask = 'noMask';
-					if (p.name == this.bouquetPic[i].name) {
-						p.mask = 'openMask';
-					}
 				}
+				p.mask = 'openMask';
 			}
 		},
 		// 花束訂購(選擇週數、配送頻率)：被選中者，checked資料改為true，其餘皆為false
@@ -181,60 +180,68 @@ Vue.createApp({
 		},
 		// 月曆：點擊後打開日期選單
 		showCal(){
-			this.calendarData.showCalBtn = 'appear';
-			this.calendarData.calMY = this.calendarData.data01.mY;
-			this.calendarData.calMYRightSwitch = 'open'
+			this.calendarData.classShowCal = 'appear';
+			this.calendarData.calMonthYear = this.calendarData.data01.monthYear;
+			this.calendarData.classRightSwitch = 'open';
 		},
 		// 月曆：點擊後關閉日期選單
 		hideCal(){
-			this.calendarData.showCalBtn = '';
+			this.calendarData.classShowCal = '';
 		},
 		// 月曆：月/年的表格切換按鈕
-		CalChangeMY(){
-			if(this.calendarData.calMYRightSwitch == 'open'){
-				this.calendarData.calMYLeftSwitch = 'open';
-				this.calendarData.calMYRightSwitch = '';
-				this.calendarData.calMY = this.calendarData.data02.mY;
+		RLSwitch(){
+			if(this.calendarData.classRightSwitch == 'open'){
+				this.calendarData.classLeftSwitch = 'open';
+				this.calendarData.classRightSwitch = '';
+				this.calendarData.calMonthYear  = this.calendarData.data02.monthYear;
 			}else{
-				this.calendarData.calMYLeftSwitch = '';
-				this.calendarData.calMYRightSwitch = 'open';
-				this.calendarData.calMY = this.calendarData.data01.mY;
+				this.calendarData.classLeftSwitch = '';
+				this.calendarData.classRightSwitch = 'open';
+				this.calendarData.calMonthYear  = this.calendarData.data01.monthYear;
 			}
 		},
-		// 月曆：填入該週的日期
-		decideDatesNum(w){
-			if(this.calendarData.calMYRightSwitch == 'open'){
+		// 月曆：填入該週的日期，並決定是否可選(加上class)
+		datesNum(w){
+			if(this.calendarData.classRightSwitch == 'open'){
 				let startIndex = (w-1)*7;
-				let aWeekDatesArray = [];
-				let dListArray =  this.calendarData.data01.d.slice(startIndex,startIndex+7);
-				for(i=0;i<dListArray.length;i++){
-					aWeekDatesArray.push({
-						d:dListArray[i],
-						c:''
+				let datesArray =  this.calendarData.data01.date.slice(startIndex,startIndex+7);
+				let datesInfoArray = [];
+				for(i=0;i<datesArray.length;i++){
+					datesInfoArray.push({
+						num:datesArray[i],
+						class:''
 					})
-					if(i == 1 && dListArray[i] != ''){
-						aWeekDatesArray[i].c = 'choose';
+					if(i == 1 && datesArray[i] != ''){
+						datesInfoArray[i].class = 'choose';
 					}
 				}
-				return aWeekDatesArray;
+				return datesInfoArray;
 			}else{
 				let startIndex = (w-1)*7;
-				let aWeekDatesArray = [];
-				let dListArray =  this.calendarData.data02.d.slice(startIndex,startIndex+7);
-				for(i=0;i<dListArray.length;i++){
-					aWeekDatesArray.push({
-						d:dListArray[i],
-						c:''
+				let datesArray =  this.calendarData.data02.date.slice(startIndex,startIndex+7);
+				let datesInfoArray = [];
+				for(i=0;i<datesArray.length;i++){
+					datesInfoArray.push({
+						num:datesArray[i],
+						class:''
 					})
-					if(i == 1 && dListArray[i] != ''){
-						aWeekDatesArray[i].c = 'choose';
+					if(i == 1 && datesArray[i] != ''){
+						datesInfoArray[i].class = 'choose';
 					}
 				}
-				return aWeekDatesArray;
+				return datesInfoArray;
 			}
 		},
-		// 月曆：用來決定該日期是不是可以選擇
+		// 月曆：用來將選入的日期，填入開始配送週的欄位，並關閉月曆表格
+		chooseDate(d){
+			this.calendarData.classShowCal = '';
 
+			if(this.calendarData.calMonthYear == this.calendarData.data01.monthYear){
+				this.bouquetText.calendar.val = '2021/12/'+d;
+			}else{
+				this.bouquetText.calendar.val = '2021/1/'+d;
+			}
+		}
 	},
 	computed: {
 		// 花束訂購(選擇週數)：用於辨別此時被選定的週數(checked=true)為何，對應到的數字是什麼。
@@ -251,30 +258,22 @@ Vue.createApp({
 			let tString = t.toString();
 			return tString.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 		},
-		// 月曆：計算週數，並轉為陣列傳回
-		data01WeeksArray(){
-			data01Weeks = this.calendarData.data01.d.length/7;
-			let data01WeeksArray = [];
-			for(i=0;i<data01Weeks;i++){
-				data01WeeksArray.splice(i,0,i+1);
-			}
-			return data01WeeksArray;
-		},
-		// 月曆：計算週數，並轉為陣列傳回
-		data02WeeksArray(){
-			data02Weeks = this.calendarData.data02.d.length/7;
-			let data02WeeksArray = [];
-			for(i=0;i<data02Weeks;i++){
-				data02WeeksArray.splice(i,0,i+1);
-			}
-			return data02WeeksArray;
-		},
-		// 月曆：用來對應月份的週數
-		decideWeeksNum(){
-			if(this.calendarData.calMYRightSwitch == 'open'){
-				return this.data01WeeksArray;
+		// 月曆：計算當月週數，並轉為陣列傳回(例如：12月有5週，則回傳[1,2,3,4,5])，用來產生與週數相同的tr
+		weeksArray(){
+			if(this.calendarData.classRightSwitch == 'open'){
+				weeksNum = this.calendarData.data01.date.length/7;
+				let weeksArray = [];
+				for(i=0;i<weeksNum;i++){
+					weeksArray.splice(i,0,i+1);
+				}
+				return weeksArray;
 			}else{
-				return this.data02WeeksArray;
+				weeksNum = this.calendarData.data02.date.length/7;
+				let weeksArray = [];
+				for(i=0;i<weeksNum;i++){
+					weeksArray.splice(i,0,i+1);
+				}
+				return weeksArray;
 			}
 		}
 	}
